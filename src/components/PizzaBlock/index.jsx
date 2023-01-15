1a1697f5d12d2195fe53fe4219057d4cf6afd9fd
d.kolbasin <d.kolbasin@kt-team.de>
2023-01-15 15:06:14 +0500

    правки стилизации копонентов

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -4,7 +4,7 @@ function PizzaBlock(props) {
   const [pizzaCount, setPizzaCount] = React.useState(0)
 
   const onClickAddButton = () => {
-    setPizzaCount(pizzaCount+1);
+    setPizzaCount(pizzaCount + 1);
   }
 
   const [activeSize, setActiveSize] = React.useState(0)
@@ -22,7 +22,7 @@ function PizzaBlock(props) {
           <ul>
             {
               props.types.map((value, index) => (
-                <li key={value + index} onClick={() => setActiveType(index)} className={activeType === index ? "active" : ""}>
+                <li key={value + index + '1'} onClick={() => setActiveType(index)} className={activeType === index ? "active" : ""}>
                   {typeName[value]}
                 </li>
               ))
@@ -31,7 +31,7 @@ function PizzaBlock(props) {
           <ul>
             {
               props.sizes.map((value, index) => (
-                <li key={value + index} onClick={() => setActiveSize(index)} className={activeSize === index ? "active" : ""}>
+                <li key={value + index + '2'} onClick={() => setActiveSize(index)} className={activeSize === index ? "active" : ""}>
                   {value} см.
                 </li>
               ))
@@ -52,9 +52,9 @@ function PizzaBlock(props) {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                fill="white"
-              ></path>
+  d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+  fill="white"
+  />
             </svg>
             <span>Добавить</span>
             <i>{pizzaCount}</i>
